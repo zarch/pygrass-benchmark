8282fc29a9e1bd0cbf30ec52e2f3740bfd1d5bfe
Pietro Zambelli <peter.zamb@gmail.com>
2013-01-25 00:10:21 +0100

    Fix some bugs

--- run.py
+++ run.py
@@ -19,10 +19,10 @@ SEEDS = [random.randint(0, 100000) for i in xrange(NVECT)]
 # raster inputs parameters
 RNAME = 'field'
 # timeit parameters
-REPEAT = 5
+REPEAT = 3
 NUMBER = 1
 #
-CREATE = True
+CREATE = False
 #---------------------------------------------------------
 # import from grass
 from grass.pygrass.modules import general as g
@@ -62,6 +62,7 @@ if CREATE:
 #
 # SAMPLE
 #
+print "Sample"
 sample_cmd = "samples(VNAMES, RNAME)"
 sample_set = '''from sample_rast import samples
 VNAMES = %r
@@ -76,6 +77,7 @@ sample_times = sample_timer.repeat(REPEAT, NUMBER)
 rr_cmd = "ifnumpy('field', 'test_ifnumpy')"
 rr_set = "from rastrow import ifnumpy"
 rr_timer = timeit.Timer(rr_cmd, setup=rr_set)
+print rr_cmd
 rr_times = rr_timer.repeat(REPEAT, NUMBER)
 
 #
@@ -84,6 +86,7 @@ rr_times = rr_timer.repeat(REPEAT, NUMBER)
 rrc_cmd = "ifnumpy_cell('field', 'test_ifnumpy_cell')"
 rrc_set = "from rastrow import ifnumpy_cell"
 rrc_timer = timeit.Timer(rrc_cmd, setup=rrc_set)
+print rrc_cmd
 rrc_times = rrc_timer.repeat(REPEAT, NUMBER)
 
 #
@@ -92,6 +95,7 @@ rrc_times = rrc_timer.repeat(REPEAT, NUMBER)
 mapcalc_cmd = """sub.Popen("r.mapcalc expression='test_mapcal=if(field>50,1,0)' --o", shell=True).wait()"""
 mapcalc_set = "import subprocess as sub"
 mapcalc_timer = timeit.Timer(mapcalc_cmd, setup=mapcalc_set)
+print mapcalc_cmd
 mapcalc_times = mapcalc_timer.repeat(REPEAT, NUMBER)
 
 #
@@ -100,39 +104,44 @@ mapcalc_times = mapcalc_timer.repeat(REPEAT, NUMBER)
 mapcalcc_cmd = """sub.Popen("r.mapcalc expression='test_mapcalc=if(field>50,field,0)' --o", shell=True).wait()"""
 mapcalcc_set = "import subprocess as sub"
 mapcalcc_timer = timeit.Timer(mapcalcc_cmd, setup=mapcalcc_set)
+print mapcalcc_cmd
 mapcalcc_times = mapcalcc_timer.repeat(REPEAT, NUMBER)
 
 #
 # Mapset glist
 #
-glist_cmd = "Mapset('PERMENENT').glist('rast')"
+glist_cmd = "Mapset('PERMANENT').glist('rast')"
 glist_set = "from grass.pygrass.gis import Mapset"
-glist_timer = timeit.Timer(mapcalcc_cmd, setup=mapcalcc_set)
-glist_times = glist_timer.repeat(REPEAT, NUMBER*10)
+glist_timer = timeit.Timer(glist_cmd, setup=glist_set)
+print glist_cmd
+glist_times = glist_timer.repeat(REPEAT, NUMBER * 10)
 
 #
 # Mapset gcore list
 #
 glistc_cmd = "gcore.list_grouped('rast')['PERMANENT']"
 glistc_set = "from grass.script import core as gcore"
-glistc_timer = timeit.Timer(mapcalcc_cmd, setup=mapcalcc_set)
-glistc_times = glistc_timer.repeat(REPEAT, NUMBER*10)
+glistc_timer = timeit.Timer(glistc_cmd, setup=glistc_set)
+print glistc_cmd
+glistc_times = glistc_timer.repeat(REPEAT, NUMBER * 10)
 
 #
 # Region to dict
 #
 reg_cmd = "dict(Region().iteritems())"
 reg_set = "from grass.pygrass.gis.region import Region"
-reg_timer = timeit.Timer(mapcalcc_cmd, setup=mapcalcc_set)
-reg_times = reg_timer.repeat(REPEAT, NUMBER*10)
+reg_timer = timeit.Timer(reg_cmd, setup=reg_set)
+print reg_cmd
+reg_times = reg_timer.repeat(REPEAT, NUMBER * 10)
 
 #
 # Region gcore to dict
 #
 regc_cmd = "gcore.region()"
-regc_set = "from grass.pygrass.gis.region import Region"
-regc_timer = timeit.Timer(mapcalcc_cmd, setup=mapcalcc_set)
-regc_times = regc_timer.repeat(REPEAT, NUMBER*10)
+regc_set = "from grass.script import core as gcore"
+regc_timer = timeit.Timer(regc_cmd, setup=regc_set)
+print regc_cmd
+regc_times = regc_timer.repeat(REPEAT, NUMBER * 0)
 
 #---------------------------------------------------------
 # Print results
